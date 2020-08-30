46c32780ada076f9e3f821c3e5932f58213cd219
otomon10 <otomono@gmail.com>
2020-08-31 01:23:05 +0900

    Add: rendering batch

---
+++ rendering.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+START_TIME=$SECONDS
+#openscad binary - just makes stuff tidy
+OPENSCAD=openscad
+PREFIX_FILE_PATH=things/custom/
+render() {
+  echo "Processing "${1}"...."
+  out_file=${PREFIX_FILE_PATH}${1}.stl
+  in_file=${PREFIX_FILE_PATH}${1}.scad
+  rm -f ${out_file}
+  ${OPENSCAD} -o ${out_file} ${in_file}
+}
+
+render "Dactyl-top-left" & \
+render "Dactyl-bottom-left" & \
+render "Dactyl-top-right" & \
+render "Dactyl-bottom-right" & \
+render "Dactyl-wrist-rest-right" & \
+render "side-sample.scad"
+
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo "finished!! execution time: $(($ELAPSED_TIME/60)) min $(($ELAPSED_TIME%60)) sec"
